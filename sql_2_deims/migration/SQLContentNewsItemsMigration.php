2b2e7f88a9363cf57c1fc8ca7b61bb9a77e8bd24
isangil <isangil@lternet.edu>
2015-05-10 17:45:15 -0600

    Improves the News Items migration

--- sql_2_deims/migration/SQLContentNewsItemsMigration.php
+++ sql_2_deims/migration/SQLContentNewsItemsMigration.php
@@ -13,7 +13,7 @@ class SQLContentNewsItemsMigration extends Migration {
     $this->map = new MigrateSQLMap(
       $this->machineName,
       array(
-        'menuID' => array('type' => 'int',
+        'NewsID' => array('type' => 'int',
                                'unsigned' => TRUE,
                                'not null' => TRUE,
                                'alias' => 'ni',
@@ -24,8 +24,7 @@ class SQLContentNewsItemsMigration extends Migration {
 
       $query = Database::getConnection('default', 'mssqlknzmeta')
                ->select('newsitems','ni')
-               ->fields('ni', array('NewsID', 'Title', 'Abstract', 'DateAdded', '
-               'Content','isActive','LinkAddress'));
+               ->fields('ni', array('NewsID', 'Title', 'DateAdded','isActive','LinkAddress'));
 
     $this->source = new MigrateSourceSQL($query);
     $this->destination = new MigrateDestinationNode('article');
@@ -34,17 +33,23 @@ class SQLContentNewsItemsMigration extends Migration {
     $this->addFieldMapping('uid')->defaultValue(1);
     $this->addFieldMapping('sticky')->defaultValue(0);
     $this->addFieldMapping('title','Title');
-    $this->addFieldMapping('body','content');
-    $this->addFieldMapping('body:format')->defaultValue('full_html');
-    $this->addFieldMapping('date','DateAdded');
+    $this->addFieldMapping('field_article_date','DateAdded');
+    $this->addFieldMapping('field_url','LinkAddress');
 
-    
+    $this->addFieldMapping('field_section')
+     ->defaultValue('News');
+
     // No Unmapped source fields
 
     // Unmapped destination fields
     $this->addUnmigratedDestinations(array(
+      'body',
+      'body:format',
       'body:summary',
       'body:language',
+      'field_article_date:timezone',
+      'field_article_date:rrule',
+      'field_article_date:to',
       'field_url:title',
       'field_url:attributes',
       'changed',
@@ -70,17 +75,15 @@ class SQLContentNewsItemsMigration extends Migration {
       'field_files:urlencode',	//Option: Encode all segments of the incoming path (defaults to TRUE). (link is external)
       'field_files:description',	//Subfield: String to be used as the description value (link is external)
       'field_files:display',	//Subfield: String to be used as the display value (link is external)
-      'field_images',	// Images (image)
-      'field_images:file_class',	//Option: Implementation of MigrateFile to use (link is external)
-       'field_images:language',	//Subfield: Language for the field
-      'field_images:preserve_files',  //	Option: Boolean indicating whether files should be preserved or deleted on rollback (link is external)
-      'field_images:destination_dir',	//Subfield: Path within Drupal files directory to store file (link is external)
-      'field_images:destination_file',	//Subfield: Path within destination_dir to store the file. (link is external)
-      'field_images:file_replace',	//Option: Value of $replace in that file function. Defaults to FILE_EXISTS_RENAME. (link is external)
-      'field_images:source_dir',	//Subfield: Path to source file. (link is external)
-      'field_images:urlencode',	//Option: Encode all segments of the incoming path (defaults to TRUE). (link is external)
-      'field_images:alt',	//Subfield: String to be used as the alt value (link is external)
-      'field_images:title',
+      'field_keywords',	//Keywords (taxonomy_term_reference)
+      'field_keywords:source_type',	//Option: Set to 'tid' when the value is a source ID (link is external)
+      'field_keywords:create_term',	//Option: Set to TRUE to create referenced terms when necessary (link is external)
+      'field_keywords:ignore_case',	//Option: Set to TRUE to ignore case differences between source data and existing term names (link is external)
+      'field_related_people',	//Contacts (entityreference)
+      'field_related_publications',	//Related publications (entityreference)
+      'field_section:source_type',	//Option: Set to 'tid' when the value is a source ID (link is external)
+      'field_section:create_term',	//Option: Set to TRUE to create referenced terms when necessary (link is external)
+      'field_section:ignore_case',
     ));
 
     if (module_exists('statistics')) {
